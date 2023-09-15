d75132575fcf5162ecd7282b6b0b565dc55465ed
Administrator <20220075@ricaldone.edu.sv>
2023-09-15 13:06:27 -0600

    infractores design fixed

---
+++ app/src/main/java/com/example/esespi/Denuncias_main.kt
@@ -0,0 +1,11 @@
+package com.example.esespi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Denuncias_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_denuncias_main)
+    }
+}

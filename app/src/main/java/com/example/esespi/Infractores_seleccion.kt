3567ffb6061eb124258bb2df2bdea6285529d023
Administrator <20220075@ricaldone.edu.sv>
2023-09-16 10:48:24 -0600

    Infractores main vista fixed

--- app/src/main/java/com/example/esespi/Infractores_seleccion.kt
+++ app/src/main/java/com/example/esespi/Infractores_seleccion.kt
@@ -87,7 +87,7 @@ class Infractores_seleccion : AppCompatActivity() {
             try {
                 //Sacamos los datos que mostraremos en la card
                 val statement = conn.createStatement()
-                val query = "SELECT * FROM dbo.Infractores_Visualizar();"
+                val query = "exec dbo.Infractores_Visualizar;"
                 val resultSet = statement.executeQuery(query)
 
                 //Sacamos los datos que obtuvimos de la busqueda sql
